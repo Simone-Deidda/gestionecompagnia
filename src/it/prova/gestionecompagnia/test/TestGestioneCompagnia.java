ddec13f12f2ed79ef4c1bfa9eb34ee6dea69bbc7
Simone-Deidda <deidda.simone94@gmail.com>
2022-05-17 20:12:06 +0200

    implementate operazioni inssrt, delete e findByExample di impiegato e compagnia insieme ai relativi test

--- src/it/prova/gestionecompagnia/test/TestGestioneCompagnia.java
+++ src/it/prova/gestionecompagnia/test/TestGestioneCompagnia.java
@@ -2,6 +2,7 @@ package it.prova.gestionecompagnia.test;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.util.List;
 
 import it.prova.gestionecompagnia.connection.MyConnection;
 import it.prova.gestionecompagnia.dao.Constants;
@@ -32,8 +33,23 @@ public class TestGestioneCompagnia {
 			System.out.println("Nella tabella Compagnia ci sono " + compagniaDAOInstance.list().size() + " elementi.");
 			System.out.println("Nella tabella Impiegato ci sono " + impiegatoDAOInstance.list().size() + " elementi.");
 			
-			testUpdateCompagnia(compagniaDAOInstance);
-			testUpdateImpiegato(impiegatoDAOInstance);
+			//testUpdateCompagnia(compagniaDAOInstance);
+			//testUpdateImpiegato(impiegatoDAOInstance);
+			System.out.println("Nella tabella Compagnia ci sono " + compagniaDAOInstance.list().size() + " elementi.");
+			System.out.println("Nella tabella Impiegato ci sono " + impiegatoDAOInstance.list().size() + " elementi.");
+			
+			//testInserimentoCompagnia(compagniaDAOInstance);
+			//testInserimentoImpiegato(impiegatoDAOInstance);
+			System.out.println("Nella tabella Compagnia ci sono " + compagniaDAOInstance.list().size() + " elementi.");
+			System.out.println("Nella tabella Impiegato ci sono " + impiegatoDAOInstance.list().size() + " elementi.");
+			
+			//testCancellazioneCompagnia(compagniaDAOInstance);
+			//testCancellazioneImpiegato(impiegatoDAOInstance);
+			System.out.println("Nella tabella Compagnia ci sono " + compagniaDAOInstance.list().size() + " elementi.");
+			System.out.println("Nella tabella Impiegato ci sono " + impiegatoDAOInstance.list().size() + " elementi.");
+			
+			testFindCompagniaByExample(compagniaDAOInstance);
+			testFindImpiegatoByExample(impiegatoDAOInstance);
 			System.out.println("Nella tabella Compagnia ci sono " + compagniaDAOInstance.list().size() + " elementi.");
 			System.out.println("Nella tabella Impiegato ci sono " + impiegatoDAOInstance.list().size() + " elementi.");
 			
@@ -98,7 +114,7 @@ public class TestGestioneCompagnia {
 	}
 	
 	public static void testInserimentoCompagnia(CompagniaDAO compagniaDAOInstance) throws Exception {
-		System.out.println("/n<<<<<<testInserimentoCompagnia: inizio>>>>>>");
+		System.out.println("\n<<<<<<testInserimentoCompagnia: inizio>>>>>>");
 		
 		Date data = new Date(new java.util.Date().getTime());
 		Compagnia compagnia = new Compagnia("ragioneSociale1", 100000000, data);
@@ -107,7 +123,104 @@ public class TestGestioneCompagnia {
 		int resultInsert = compagniaDAOInstance.insert(compagnia);
 		if (resultInsert < 1)
 			throw new RuntimeException("testInserimentoCompagnia : FAILED");
+		else System.out.println("testInserimentoCompagnia : SUCCESS");
 		
-		System.out.println("<<<<<<testInserimentoCompagnia: fine>>>>>>");
+		System.out.println("<<<<<<testInserimentoCompagnia: fine>>>>>>\n");
+	}
+	
+	public static void testInserimentoImpiegato(ImpiegatoDAO impiegatoDAOInstance) throws Exception {
+		System.out.println("\n<<<<<<testInserimentoImpiegato: inizio>>>>>>");
+		
+		Date data = new Date(new java.util.Date().getTime());
+		Impiegato impiegato = new Impiegato("nome1", "cognome1", "NNNCCC11A22Z", data,  data);
+		impiegato.setId((long) impiegatoDAOInstance.list().size() + 1);
+		
+		int resultInsert = impiegatoDAOInstance.insert(impiegato);
+		if (resultInsert < 1)
+			throw new RuntimeException("testInserimentoImpiegato : FAILED");
+		else System.out.println("testInserimentoImpiegato : SUCCESS");
+		
+		System.out.println("<<<<<<testInserimentoImpiegato: fine>>>>>>\n");
+	}
+	
+	public static void testCancellazioneImpiegato(ImpiegatoDAO impiegatoDAOInstance) throws Exception {
+		System.out.println("\n<<<<<<testCancellazioneImpiegato: inizio>>>>>>");
+		
+		Impiegato primoImpiegato = impiegatoDAOInstance.list().get(0);
+		
+		int resultDelete = impiegatoDAOInstance.delete(primoImpiegato);
+		if (resultDelete < 1)
+			throw new RuntimeException("testCancellazioneImpiegato : FAILED");
+		else System.out.println("testCancellazioneImpiegato : SUCCESS");
+		
+		System.out.println("<<<<<<testCancellazioneImpiegato: fine>>>>>>\n");
+	}
+	
+	public static void testCancellazioneCompagnia(CompagniaDAO compagniaDAOInstance) throws Exception {
+		System.out.println("\n<<<<<<testCancellazioneCompagnia: inizio>>>>>>");
+		
+		Date data = new Date(new java.util.Date().getTime());
+		Compagnia daCancellare = new Compagnia("ragioneSoc", 10000000, data);
+		daCancellare.setId((long) (compagniaDAOInstance.list().size() + 1));
+		
+		int resultDelete = compagniaDAOInstance.insert(daCancellare);
+		resultDelete = compagniaDAOInstance.delete(daCancellare);
+		if (resultDelete < 1)
+			throw new RuntimeException("testCancellazioneCompagnia : FAILED");
+		else System.out.println("testCancellazioneCompagnia : SUCCESS");
+		
+		System.out.println("<<<<<<testCancellazioneCompagnia: fine>>>>>>\n");
+	}
+	
+	public static void testFindImpiegatoByExample(ImpiegatoDAO impiegatoDAOInstance) throws Exception {
+		System.out.println("\n<<<<<<testFindImpiegatoByExample: inizio>>>>>>");
+		
+		// con nessuna inizializzazione nei dati dovrebbe ritornare la ista intera 
+		List<Impiegato> listaImpiegatiTrovati;
+		Impiegato example = new Impiegato();
+		listaImpiegatiTrovati = impiegatoDAOInstance.findByExample(example);
+		
+		if (listaImpiegatiTrovati.size() != impiegatoDAOInstance.list().size())
+			throw new RuntimeException("testFindImpiegatoByExample : FAILED");
+		else System.out.println("testFindImpiegatoByExample: SUCCESS");
+		
+		// con nome inizializzato dovrebbe ritornare elementi con almeno l'iniziale uguale al nome
+		String iniziale = "no";
+		example.setNome(iniziale);
+		listaImpiegatiTrovati = impiegatoDAOInstance.findByExample(example);
+		
+		for (Impiegato impiegato : listaImpiegatiTrovati) {
+			System.out.println(impiegato);
+			if(!impiegato.getNome().startsWith(iniziale))
+				throw new RuntimeException("testFindImpiegatoByExample : FAILED");
+		}
+		
+		System.out.println("<<<<<<testFindImpiegatoByExample: fine>>>>>>\n");
+	}
+	
+	public static void testFindCompagniaByExample(CompagniaDAO compagniaDAOInstance) throws Exception {
+		System.out.println("\n<<<<<<testFindCompagniaByExample: inizio>>>>>>");
+		
+		// con nessuna inizializzazione nei dati dovrebbe ritornare la ista intera 
+		List<Compagnia> listaCompagnieTrovati;
+		Compagnia example = new Compagnia();
+		listaCompagnieTrovati = compagniaDAOInstance.findByExample(example);
+		
+		if (listaCompagnieTrovati.size() != compagniaDAOInstance.list().size())
+			throw new RuntimeException("testFindCompagniaByExample : FAILED");
+		else System.out.println("testFindCompagniaByExample: SUCCESS");
+		
+		// con nome inizializzato dovrebbe ritornare elementi con almeno l'iniziale uguale al nome
+		String iniziale = "ra";
+		example.setRagioneSociale(iniziale);
+		listaCompagnieTrovati = compagniaDAOInstance.findByExample(example);
+		
+		for (Compagnia compagnia : listaCompagnieTrovati) {
+			System.out.println(compagnia);
+			if(!compagnia.getRagioneSociale().startsWith(iniziale))
+				throw new RuntimeException("testFindCompagniaByExample : FAILED");
+		}
+		
+		System.out.println("<<<<<<testFindCompagniaByExample: fine>>>>>>\n");
 	}
 }
